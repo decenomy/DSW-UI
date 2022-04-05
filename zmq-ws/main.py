65b592a9509d6a92c6b2fb78d2260e9ad9323d46
stepollo2 <sbrasoinfinito@gmail.com>
2022-04-05 18:35:30 +0200

    working zmq=>ws implementation

---
+++ zmq-ws/main.py
@@ -0,0 +1,43 @@
+import array
+import binascii
+import zmq
+import struct
+import websockets
+import asyncio
+
+port = 29000
+
+zmqContext = zmq.Context()
+zmqSubSocket = zmqContext.socket(zmq.SUB)
+zmqSubSocket.setsockopt(zmq.SUBSCRIBE, b"hashblock")
+zmqSubSocket.setsockopt(zmq.SUBSCRIBE, b"hashtx")
+zmqSubSocket.connect("tcp://127.0.0.1:%i" % port)
+
+uri = "ws://localhost:8765"
+async def zero():
+
+    try:
+        while True:
+            msg = zmqSubSocket.recv_multipart()
+            topic = str(msg[0].decode("utf-8"))
+            body = msg[1]
+            sequence = "Unknown";
+
+            if len(msg[-1]) == 4:
+              msgSequence = struct.unpack('<I', msg[-1])[-1]
+              sequence = str(msgSequence)
+
+            if topic == "hashblock":
+                block = binascii.hexlify(body).decode("utf-8")
+                async with websockets.connect(uri) as websocket:
+                    await websocket.send(block)
+            elif topic == "hashtx":
+                tx = binascii.hexlify(body).decode("utf-8")
+                async with websockets.connect(uri) as websocket:
+                    await websocket.send(tx)
+
+    except KeyboardInterrupt:
+        zmqContext.destroy()
+
+if __name__ == "__main__":
+    asyncio.run(zero())
