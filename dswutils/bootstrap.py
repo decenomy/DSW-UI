bb91326b889e428b1fe9fae0886ea3d100f802ee
stepollo2 <sbrasoinfinito@gmail.com>
2022-04-05 18:47:17 +0200

    fix path for win bootstrap

--- dswutils/bootstrap.py
+++ dswutils/bootstrap.py
@@ -23,14 +23,14 @@ def wipe(coin):
     plt = platform.system()
     if   plt == "Windows":
         home = os.path.expanduser("~")
-        for i in listdir(home + "\AppData\Roaming\\" + coin.capitalize() + "blocks" ):
-            os.remove(os.path.join(home + "\AppData\Roaming\\" + coin.capitalize() + "blocks" , i))
-        for i in listdir(home + "\AppData\Roaming\\" + coin.capitalize() + "chainstate" ):
-            os.remove(os.path.join(home + "\AppData\Roaming\\" + coin.capitalize() + "chainstate" , i))
-        os.rmdir(home + "\AppData\Roaming\\" + coin.capitalize() + "blocks" )
-        os.rmdir(home + "\AppData\Roaming\\" + coin.capitalize() + "chainstate" )
+        for i in listdir(home + "\\AppData\\Roaming\\" + coin.capitalize() + "blocks" ):
+            os.remove(os.path.join(home + "\\AppData\\Roaming\\" + coin.capitalize() + "blocks" , i))
+        for i in listdir(home + "\\AppData\\Roaming\\" + coin.capitalize() + "chainstate" ):
+            os.remove(os.path.join(home + "\\AppData\\Roaming\\" + coin.capitalize() + "chainstate" , i))
+        os.rmdir(home + "\\AppData\\Roaming\\" + coin.capitalize() + "blocks" )
+        os.rmdir(home + "\\AppData\\Roaming\\" + coin.capitalize() + "chainstate" )
         with zipfile.ZipFile("bootstrap.zip", "r") as zip_ref:
-            zip_ref.extractall(home + "\AppData\Roaming\\" + coin.capitalize())
+            zip_ref.extractall(home + "\\AppData\\Roaming\\" + coin.capitalize())
     elif plt == "Linux":
         home = os.path.expanduser("~")
         os.system("rm -rf " + home + "/.." + coin + "/chainstate")
