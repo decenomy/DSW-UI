6f681fd6e4a6d41fdfe8da260cb297031dafc4e5
Caprica01 <antoninimichele8@gmail.com>
2022-04-26 14:42:40 +0200

    fix win bootstrap

--- dswutils/bootstrap.py
+++ dswutils/bootstrap.py
@@ -2,6 +2,7 @@ import requests
 import platform
 import zipfile
 import os
+import shutil
 
 def bdownload(url, fname):
     resp = requests.get(url, stream=True)
@@ -15,12 +16,8 @@ def wipe(coin):
     plt = platform.system()
     if  plt == "Windows":
         home = os.path.expanduser("~")
-        for i in os.listdir(home + "\\AppData\\Roaming\\" + coin.capitalize() + "\\blocks" ):
-            os.remove(os.path.join(home + "\\AppData\\Roaming\\" + coin.capitalize() + "\\blocks" , i))
-        for i in os.listdir(home + "\\AppData\\Roaming\\" + coin.capitalize() + "\\chainstate" ):
-            os.remove(os.path.join(home + "\\AppData\\Roaming\\" + coin.capitalize() + "\\chainstate" , i))
-        os.rmdir(home + "\\AppData\\Roaming\\" + coin.capitalize() + "\\blocks" )
-        os.rmdir(home + "\\AppData\\Roaming\\" + coin.capitalize() + "\\chainstate" )
+        shutil.rmtree(home + "\\AppData\\Roaming\\" + coin.capitalize() + "\\blocks" )
+        shutil.rmtree(home + "\\AppData\\Roaming\\" + coin.capitalize() + "\\chainstate" )
         with zipfile.ZipFile("bootstrap.zip", "r") as zip_ref:
             zip_ref.extractall(home + "\\AppData\\Roaming\\" + coin.capitalize())
         os.remove("bootstrap.zip")
