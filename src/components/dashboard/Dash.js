d87b209bf47d82f96d9fa2137ab03dfa50e80f49
stepollo2 <sbrasoinfinito@gmail.com>
2022-05-04 15:11:09 +0200

    add getinfo ws

--- src/components/dashboard/Dash.js
+++ src/components/dashboard/Dash.js
@@ -59,15 +59,13 @@ export function Dash() {
         else if (mydata["type"] === "transaction") {
           console.log("new tx");
         }
-        /*
         else if (mydata["type"] == "getinfo") {
-        const updatedGetInfo = [...getInfo];
+        const updatedGetInfo = {...getInfo};
         updatedGetInfo["staking status"] = mydata["data"]["staking status"];
-        updatedGetInfo["balance"] = mydata["data"]["balance"];
-        updatedGetInfo["connections"] = mydata["data"]["connections"];
+        updatedGetInfo.balance = mydata["data"]["balance"];
+        updatedGetInfo.connections = mydata["data"]["connections"];
         setGetInfo(updatedGetInfo);
         }
-        */
         else if (mydata["type"] === "masternodes") {
           console.log("mn");
         }
