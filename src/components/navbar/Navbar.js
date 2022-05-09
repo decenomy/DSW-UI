012412ef209967ca6febfdb9c2e04d40ce13388c
stepollo2 <sbrasoinfinito@gmail.com>
2022-05-09 16:50:06 +0200

    toggle menu onclick for mobile

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {  useState } from 'react';
 import { Link } from 'react-router-dom'
 
 import logo from 'logo.svg';
 
 export function Navbar() {
+
+  const [isActive, setisActive] = useState(false)
+
   return (
 
 <nav className="navbar" role="navigation" aria-label="main navigation">
@@ -12,14 +15,16 @@ export function Navbar() {
       <img src={logo} width="112" height="28" />
     </a>
 
-    <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+    <a role="button" onClick={() => {
+            setisActive(!isActive)
+          }} className={`navbar-burger burger ${isActive ? 'is-active' : ''}`} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
   </div>
 
-  <div id="navbarBasicExample" className="navbar-menu">
+  <div id="navbarBasicExample" className={`navbar-menu ${isActive ? 'is-active' : ''}`}>
     <div className="navbar-start">
      
       <Link className="navbar-item" to='/dash'>Dashboard</Link>
