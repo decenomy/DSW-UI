f95b08e3758f519d1f06cfa61dc92febcfd2f9f2
stepollo2 <sbrasoinfinito@gmail.com>
2022-05-09 16:28:55 +0200

    make the copy button working

--- src/components/transactions/ReceivePage.js
+++ src/components/transactions/ReceivePage.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { get } from 'utils/requests';
 import Navbar from 'components/navbar/Navbar';
 import {QRCodeCanvas} from 'qrcode.react';
+import { ToastContainer, toast } from 'react-toastify';
 
 export function ReceivePage() {
 
@@ -14,10 +15,19 @@ const [getNewAddr, setGetNewAddr] = useState([]);
       (error) => console.error(error)
     )
   }, []);
-
+  const textAreaRef = useRef(null);
+  function copyToClipboard(e) {
+    textAreaRef.current.select();
+    document.execCommand('copy');
+    // This is just personal preference.
+    // I prefer to not show the the whole text area selected.
+    e.target.focus();
+    toast.success("Address copied!");
+  };
 
 return (
 <div className='container'>
+<ToastContainer />
 <Navbar />
 <div className="columns">
 <div className="column is-one-fifth">
@@ -28,10 +38,10 @@ return (
 <div className="field">
     <label className="label">Receive</label>
     <div className="control">
-    <input className="input" type="text" value={getNewAddr["address"]} id="address" readonly />
+    <input className="input" ref={textAreaRef} type="text" value={getNewAddr["address"]} id="address" readonly />
     </div>
 </div>
-<input className="button is-info is-rounded is-outlined" type="submit" value="Copy" id="copy-button" />
+<input className="button is-info is-rounded is-outlined" onClick={copyToClipboard} type="submit" value="Copy" id="copy-button" />
 </div>
 </div>
 
